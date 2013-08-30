2cf50e32f20b4b506ea893b19b32212f5539b80c
Josh Long <josh@joshlong.com>
2013-08-30 13:38:59 -0700

    cleanuo

--- code/web/services/src/main/java/com/jl/crm/services/security/CrmUserDetailsService.java
+++ code/web/services/src/main/java/com/jl/crm/services/security/CrmUserDetailsService.java
@@ -2,84 +2,85 @@ package com.jl.crm.services.security;
 
 import com.jl.crm.services.CrmService;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import javax.inject.Inject;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
-@Component ("userService")
+@Component("userService")
 public class CrmUserDetailsService implements UserDetailsService {
 
-	private CrmService crmService;
+    private CrmService crmService;
 
-	@Inject
-	public CrmUserDetailsService(CrmService crmService) {
-		this.crmService = crmService;
-	}
+    @Inject
+    public CrmUserDetailsService(CrmService crmService) {
+        this.crmService = crmService;
+    }
 
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		com.jl.crm.services.User user = crmService.findUserByUsername(username);
-		return new CrmUserDetails(user);
-	}
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.jl.crm.services.User user = crmService.findUserByUsername(username);
+        return new CrmUserDetails(user);
+    }
 
-	@SuppressWarnings("serial")
-	public static class CrmUserDetails implements UserDetails {
-		public static final String SCOPE_READ = "read";
-		public static final String SCOPE_WRITE = "write";
-		public static final String ROLE_USER = "ROLE_USER";
-		private Collection<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
-		private com.jl.crm.services.User user;
+    @SuppressWarnings("serial")
+    public static class CrmUserDetails implements UserDetails {
+        public static final String SCOPE_READ = "read";
+        public static final String SCOPE_WRITE = "write";
+        public static final String ROLE_USER = "ROLE_USER";
+        private Collection<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+        private com.jl.crm.services.User user;
 
-		public CrmUserDetails(com.jl.crm.services.User user) {
-			Assert.notNull(user, "the provided user reference can't be null");
-			this.user = user;
-			for (String ga : Arrays.asList(ROLE_USER, SCOPE_READ, SCOPE_WRITE)) {
-				this.grantedAuthorities.add(new SimpleGrantedAuthority(ga));
-			}
-		}
+        public CrmUserDetails(com.jl.crm.services.User user) {
+            Assert.notNull(user, "the provided user reference can't be null");
+            this.user = user;
+            this.grantedAuthorities = AuthorityUtils.createAuthorityList(ROLE_USER, SCOPE_READ, SCOPE_WRITE);
+        }
 
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			return this.grantedAuthorities;
-		}
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return this.grantedAuthorities;
+        }
 
-		@Override
-		public String getPassword() {
-			return user.getPassword();
-		}
+        @Override
+        public String getPassword() {
+            return user.getPassword();
+        }
 
-		@Override
-		public String getUsername() {
-			return user.getUsername();
-		}
+        @Override
+        public String getUsername() {
+            return user.getUsername();
+        }
 
-		@Override
-		public boolean isAccountNonExpired() {
-			return isEnabled();
-		}
+        @Override
+        public boolean isAccountNonExpired() {
+            return isEnabled();
+        }
 
-		@Override
-		public boolean isAccountNonLocked() {
-			return isEnabled();
-		}
+        @Override
+        public boolean isAccountNonLocked() {
+            return isEnabled();
+        }
 
-		@Override
-		public boolean isCredentialsNonExpired() {
-			return isEnabled();
-		}
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return isEnabled();
+        }
 
-		@Override
-		public boolean isEnabled() {
-			return user.isEnabled();
-		}
+        @Override
+        public boolean isEnabled() {
+            return user.isEnabled();
+        }
 
-		public com.jl.crm.services.User getUser() {
-			return this.user;
-		}
-	}
+        public com.jl.crm.services.User getUser() {
+            return this.user;
+        }
+    }
 
 }

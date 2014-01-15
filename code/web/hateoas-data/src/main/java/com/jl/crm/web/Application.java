f93d3f66990d754d56e3caef31450951ef639668
Josh Long <josh@joshlong.com>
2014-01-15 04:50:59 -0800

    adding application for hateoas-data move to Boot

---
+++ code/web/hateoas-data/src/main/java/com/jl/crm/web/Application.java
@@ -0,0 +1,60 @@
+package com.jl.crm.web;
+
+import com.jl.crm.services.ServiceConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.util.UriTemplate;
+
+import javax.servlet.MultipartConfigElement;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
+    private static Class<Application> applicationClass = Application.class;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass);
+    }
+}
+
+
+@Configuration
+@Import({ServiceConfiguration.class, RepositoryRestMvcConfiguration.class})
+@EnableWebMvc
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
+class WebMvcConfiguration {
+
+    String CURIE_NAMESPACE = "crm";
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        return new MultipartConfigElement("");
+    }
+
+    @Bean
+    MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    DefaultCurieProvider defaultCurieProvider() {
+        return new DefaultCurieProvider(CURIE_NAMESPACE, new UriTemplate("http://localhost:8080/rels/{rel}"));
+    }
+}

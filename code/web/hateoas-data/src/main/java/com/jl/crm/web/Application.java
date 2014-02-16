da0c6d5c98a1988189bc1043c554f0308ba438a4
Josh Long <josh@joshlong.com>
2014-02-16 07:49:37 -0800

    moved even more over to Boot. Managed to get services and rest working.

--- code/web/hateoas-data/src/main/java/com/jl/crm/web/Application.java
+++ code/web/hateoas-data/src/main/java/com/jl/crm/web/Application.java
@@ -1,11 +1,8 @@
 package com.jl.crm.web;
 
-import javax.servlet.MultipartConfigElement;
-
+import com.jl.crm.services.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,45 +13,39 @@ import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.jl.crm.services.ServiceConfiguration;
+import javax.servlet.MultipartConfigElement;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
-	private static Class<Application> applicationClass = Application.class;
+public class Application {
+    private static Class<Application> applicationClass = Application.class;
 
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(applicationClass);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass);
+    }
 }
 
 @Configuration
-@Import({ ServiceConfiguration.class, RepositoryRestMvcConfiguration.class })
+@Import({ServiceConfiguration.class, RepositoryRestMvcConfiguration.class})
 @EnableWebMvc
 class WebMvcConfiguration {
 
-	String curieNamespace = "crm";
+    String curieNamespace = "crm";
 
-	@Bean
-	MultipartConfigElement multipartConfigElement() {
-		return new MultipartConfigElement("");
-	}
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        return new MultipartConfigElement("");
+    }
 
-	@Bean
-	MultipartResolver multipartResolver() {
-		return new StandardServletMultipartResolver();
-	}
+    @Bean
+    MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 
-	@Bean
-	DefaultCurieProvider defaultCurieProvider() {
-		org.springframework.hateoas.UriTemplate template = new org.springframework.hateoas.UriTemplate(
-				"http://localhost:8080/rels/{rel}");
-		return new DefaultCurieProvider(curieNamespace, template);
-	}
+    @Bean
+    DefaultCurieProvider defaultCurieProvider() {
+        org.springframework.hateoas.UriTemplate template = new org.springframework.hateoas.UriTemplate(
+                "http://localhost:8080/rels/{rel}");
+        return new DefaultCurieProvider(curieNamespace, template);
+    }
 }
